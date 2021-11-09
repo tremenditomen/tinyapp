7701751f809d765cf7892009cb636d2194e3b2a4
Luis Andres Contreras <tremenditomen@gmail.com>
2021-11-09 14:00:59 -0700

    added urls_news.ejs

--- express-server.js
+++ express-server.js
@@ -6,6 +6,10 @@ const PORT = 8080; // default port 8080
 app.set('view engine', 'ejs')
 app.use(morgan('dev'))
 app.use(express.urlencoded({extended: false}))
+function generateRandomString() {
+
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -26,6 +30,10 @@ app.get("/hello", (req, res) => {
     const templateVars = { urls: urlDatabase };
     res.render("urls_index",templateVars)
   })
+  //APP.GET FOR THE FORM
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
   //single url
   app.get("/urls/:shortURL", (req, res) => {
       const shortURL = req.params.shortURL
@@ -33,6 +41,12 @@ app.get("/hello", (req, res) => {
     const templateVars = { shortURL: shortURL, longURL: longURL};
     res.render("urls_show", templateVars);
   });
+  //APP.POST for newURLS
+  app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
+  
   
 
 app.listen(PORT, () => {
